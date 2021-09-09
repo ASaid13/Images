e9648dbd1f02f518c1a427e8972c3950e9b48b14
ASaid13 <86315255+ASaid13@users.noreply.github.com>
2021-09-09 11:28:17 -0700

    Update Hacker.sh

--- Hacker.sh
+++ Hacker.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-
+INPUT_STRING=unicorn
+while [ "$INPUT_STRING" != "end" ]
+do
+cd /Images
+img2txt -W 50 513KLIb5CwL.jpg
+img2txt -W 100 71b9p7Y3UhL.png
+img2txt -W 100 81vwzxcPtxL.png
+done
 
 
